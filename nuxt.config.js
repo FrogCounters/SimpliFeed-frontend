f2c598186ae97957bdcf69aa303dd001dd6ba7af
Guan Quan <76832850+guanquann@users.noreply.github.com>
2023-01-16 22:45:42 +0800

    update header title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'simplifeed-frontend',
+    title: 'SimpliFeed',
     htmlAttrs: {
       lang: 'en',
     },
